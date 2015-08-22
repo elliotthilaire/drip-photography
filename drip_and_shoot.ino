4ea27214878e50190fd0be359771168ef2a880f4
elliotthilaire <elliott.hilaire@gmail.com>
2015-08-22 13:56:18 +1000

    use millis() for more async like delay

--- drip_and_shoot.ino
+++ drip_and_shoot.ino
@@ -1,79 +1,102 @@
 
-
-int SOLENOID_PIN = 2;
+int SOLENOID_VALVE_PIN = 2;
 int IR_PIN = 12;
 int STATUS_LED = 13;
 
+boolean initial_drop_released;
+boolean collision_drop_released;
+boolean photo_taken;
 
-// the setup routine runs once when you press reset:
+int start_time;
+
 void setup() {
-  pinMode(SOLENOID_PIN, OUTPUT);
+  pinMode(SOLENOID_VALVE_PIN, OUTPUT);
   pinMode(IR_PIN, OUTPUT);
   pinMode(STATUS_LED, OUTPUT);
-}
+  
+  initial_drop_released = false;
+  collision_drop_released = false;
+  photo_taken = false;
+  
+  Serial.begin(9600);
+  
+  int start_time = millis();
 
-// the loop routine runs over and over again forever:
-int count = 0;
+  log("Setup finished at: ", millis());
+
+}
 
 void loop() {
-  while(count<2){
+  
+  int time = millis();
+    
+  if (time > 0 and initial_drop_released == false ) {
+    log("Initial Drip started at: ", millis());
+    
     drip();
-    wait_for_gravity();
-    take_photo();
-    wait_for_next_photo();
-    count ++;
+    initial_drop_released = true;
+    
+    log("Initial Drip ended at: ", millis());
   }
   
-  digitalWrite(STATUS_LED, HIGH);
+  if (time > 80 and collision_drop_released == false) {
+    log("Collision Drip started at: ", millis());
+
+    drip();
+    collision_drop_released = true;
+    
+    log("Collision Drip ended at: ", millis());
+  }
+
+  if (time > 300 and photo_taken == false) {
+    log("Start take photo: ", millis());
+    
+    take_photo();
+    photo_taken = true;
+    
+    log("End take photo: ", millis());
+    digitalWrite(STATUS_LED, HIGH);
+  }
+
 }
 
-void wait_for_gravity(){
-  // about 300ms to fall 50cm
-  // as taken from here http://www.gravitycalc.com/
-  delay(300);
-}
 
-void wait_for_next_photo(){
-  delay(10000);
+// log method to correct concatinate different types
+// i.e., string and int
+void log(String message, int time){
+  String output = message + time;
+  Serial.println(output);
 }
 
 void drip(){
-  int SMALLEST_DROP = 70;
-  int SMALLEST_DELAY = 120;
+  int OPEN_VALVE_DURATION = 70;
 
-  digitalWrite(SOLENOID_PIN, HIGH);
-  delay(SMALLEST_DROP);
-  digitalWrite(SOLENOID_PIN, LOW);
-  delay(SMALLEST_DELAY);
-
-  digitalWrite(SOLENOID_PIN, HIGH);
-  delay(SMALLEST_DROP);
-  digitalWrite(SOLENOID_PIN, LOW);
+  digitalWrite(SOLENOID_VALVE_PIN, HIGH);
+  delay(OPEN_VALVE_DURATION);
+  digitalWrite(SOLENOID_VALVE_PIN, LOW); 
 }
 
-
 void take_photo(){
-
   int count = 0;
 
-  while(count<3) {
-    tone(IR_PIN,38000);
+  while(count < 3) {
+    tone(IR_PIN, 38000);
     delay(2);
     noTone(IR_PIN);
     delay(28);
-    tone(IR_PIN,38000);
+    tone(IR_PIN, 38000);
     delayMicroseconds(200);
     noTone(IR_PIN);
     delayMicroseconds(1500);
-    tone(IR_PIN,38000);
+    tone(IR_PIN, 38000);
     delayMicroseconds(200);
     noTone(IR_PIN);
     delayMicroseconds(3300);
-    tone(IR_PIN,38000);
+    tone(IR_PIN, 38000);
     delayMicroseconds(200);
     noTone(IR_PIN);
     delayMicroseconds(100);
-    delay(IR_PIN);
+    delay(63);
     count++;
   }
 }
